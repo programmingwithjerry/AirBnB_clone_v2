150a17226ee5bf87c08ba4f57bd4867c841d833f
programmingwithjerry <eneyellowj@gmail.com>
2024-07-31 18:08:09 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,19 +7,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Handles requests to the root URL and returns a greeting message.
-    
     Returns:
         str: The greeting message 'Hello HBNB!'.
     """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
     """
-    Main entry point of the application. 
+    Main entry point of the application.
     Runs the Flask app on all available IP addresses at port 5000.
     """
     app.run(host='0.0.0.0', port=5000)
